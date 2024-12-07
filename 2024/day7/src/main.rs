dca85aaafa47689373f0010c12be97e283489752
Paweł Cyman <pawel@cyman.xyz>
2024-12-07 11:51:56 +0100

    2024 day 7 (improve perf)
    2024 day 7 (improve perf)

--- 2024/day7/src/main.rs
+++ 2024/day7/src/main.rs
@@ -1,4 +1,3 @@
-use itertools::Itertools;
 use std::fs::File;
 use std::io::{self, BufRead};
 
@@ -6,20 +5,22 @@ fn main() {
     let file = File::open("input.txt").expect("Could not open input file");
     let reader = io::BufReader::new(file);
 
-    let mut equations: Vec<(usize, Vec<usize>)> = Vec::new();
-
-    reader.lines().into_iter().for_each(|line| {
-        let line = line.expect("Failed to read line");
-        let mut split = line.split(":").map(|x| x.trim());
-        let result = split.next().unwrap().parse::<usize>().unwrap();
-        let numbers = split
-            .next()
-            .unwrap()
-            .split_whitespace()
-            .map(|x| x.parse::<usize>().unwrap())
-            .collect();
-        equations.push((result, numbers));
-    });
+    let equations: Vec<(usize, Vec<usize>)> = reader
+        .lines()
+        .into_iter()
+        .map(|line| {
+            let line = line.expect("Failed to read line");
+            let mut split = line.split(":").map(|x| x.trim());
+            let result = split.next().unwrap().parse::<usize>().unwrap();
+            let numbers = split
+                .next()
+                .unwrap()
+                .split_whitespace()
+                .map(|x| x.parse::<usize>().unwrap())
+                .collect();
+            (result, numbers)
+        })
+        .collect();
 
     part1(equations.clone());
     part2(equations);
@@ -28,24 +29,7 @@ fn main() {
 fn part1(equations: Vec<(usize, Vec<usize>)>) {
     let sum: usize = equations
         .iter()
-        .filter(|(result, numbers)| {
-            let possible_operators = Vec::from(['+', '*']);
-            let possible_operator_configurations =
-                std::iter::repeat_n(possible_operators, numbers.len())
-                    .multi_cartesian_product()
-                    .collect::<Vec<_>>();
-            possible_operator_configurations.iter().any(|operators| {
-                let mut iteration_result = numbers[0];
-                for (operator, number) in operators.iter().zip(numbers.iter().skip(1)) {
-                    match operator {
-                        '+' => iteration_result += number,
-                        '*' => iteration_result *= number,
-                        _ => panic!("Invalid operator"),
-                    }
-                }
-                iteration_result == *result
-            })
-        })
+        .filter(|(result, numbers)| is_valid(&numbers[1..].to_vec(), numbers[0], *result))
         .map(|(result, _)| result)
         .sum();
     println!("Part 1: {}", sum);
@@ -54,30 +38,43 @@ fn part1(equations: Vec<(usize, Vec<usize>)>) {
 fn part2(equations: Vec<(usize, Vec<usize>)>) {
     let sum: usize = equations
         .iter()
-        .filter(|(result, numbers)| {
-            let possible_operators = Vec::from(["+", "*", "||"]);
-            let possible_operator_configurations =
-                std::iter::repeat_n(possible_operators, numbers.len())
-                    .multi_cartesian_product()
-                    .collect::<Vec<_>>();
-            possible_operator_configurations.iter().any(|operators| {
-                let mut iteration_result = numbers[0];
-                for (operator, number) in operators.iter().zip(numbers.iter().skip(1)) {
-                    match operator {
-                        &"+" => iteration_result += number,
-                        &"*" => iteration_result *= number,
-                        &"||" => {
-                            let mut a = iteration_result.to_string();
-                            a.push_str(number.to_string().as_str());
-                            iteration_result = a.parse::<usize>().unwrap();
-                        }
-                        _ => panic!("Invalid operator"),
-                    }
-                }
-                iteration_result == *result
-            })
-        })
+        .filter(|(result, numbers)| is_valid_p2(&numbers[1..].to_vec(), numbers[0], *result))
         .map(|(result, _)| result)
         .sum();
     println!("Part 2: {}", sum);
+}
+
+fn is_valid(numbers: &Vec<usize>, result_candidate: usize, result: usize) -> bool {
+    if numbers.is_empty() || result_candidate > result {
+        return result_candidate == result;
+    }
+
+    is_valid(
+        &numbers[1..].to_vec(),
+        numbers[0] + result_candidate,
+        result,
+    ) || is_valid(
+        &numbers[1..].to_vec(),
+        numbers[0] * result_candidate,
+        result,
+    )
+}
+
+fn is_valid_p2(numbers: &Vec<usize>, result_candidate: usize, result: usize) -> bool {
+    if numbers.is_empty() || result_candidate > result {
+        return result_candidate == result;
+    }
+
+    let mut a = result_candidate.to_string();
+    a.push_str(numbers[0].to_string().as_str());
+
+    is_valid_p2(
+        &numbers[1..].to_vec(),
+        numbers[0] + result_candidate,
+        result,
+    ) || is_valid_p2(
+        &numbers[1..].to_vec(),
+        numbers[0] * result_candidate,
+        result,
+    ) || is_valid_p2(&numbers[1..].to_vec(), a.parse::<usize>().unwrap(), result)
 }
